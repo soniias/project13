b7daef36702c6249a7a81b4dcc175a3da3329835
TiD <nsofi2009@gmail.com>
2023-11-18 10:12:49 +0200

    13

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import sqlite3
 
-connection = sqlite3.connect("student_DB.db", 5)
+connection = sqlite3.connect("Student_DB.db", 5)
 cur = connection.cursor()
-print(connection)
-print(cur)
+
+# print(connection)
+# print(cur)
+
+# cur.execute("CREATE TABLE person (name TEXT, lastname TEXT, age INTEGER, phone TEXT);")
+cur.execute("INSERT INTO person (name, lastname, age, phone) VALUES ('Nick', 'Solovi', 25, '+380123456789');")
+
+cur.execute("SELECT * FROM person WHERE name IS 'Nick';")
+
+for row in cur:
+    print(row)
+
+connection.close()
